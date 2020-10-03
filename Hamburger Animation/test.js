097a3aa808969e32e57d21996f56bacc09aa9bbf
Asif Ahmed <64367845+AsifAhmed786@users.noreply.github.com>
2020-10-03 15:42:55 +0500

    slider effect

---
+++ Hamburger Animation/test.js
@@ -0,0 +1,28 @@
+let divs = document.querySelector(".divs")
+let status = false
+// divs.addEventListener("click",()=>{
+    
+//     divs.style.transform += "rotate(45deg)";
+//     if(status){
+//         divs.classList.remove("change")
+//         status = false
+//         console.log("true")
+//         // divs.style.display = "none"
+//     }
+//     else{
+//         divs.classList.add("change")
+//         status = true
+//         console.log("false")
+//     }
+// })
+    // let rotate = ()=>{
+    //     divs.style.transform += "translateX(20px)"
+    // }
+    // let interval = setInterval(rotate,1000)
+    // setTimeout(() => {
+    //     divs.style.display = "block"
+    //     clearInterval(interval)    
+    // }, 10000);
+
+    divs.classList.add("change")
+
