f9c1ccc9c430d1c5c89e421ba5dc72ad5c04f4b7
Asif Ahmed <64367845+AsifAhmed786@users.noreply.github.com>
2020-10-10 16:42:34 +0500

    practice

---
+++ Animation/app.js
@@ -0,0 +1,9 @@
+// let btn1 = document.querySelector("btn1")
+let div = document.getElementById("maindiv")
+let btn1 = document.getElementById("btn1")
+ btn1.addEventListener("click",()=>{
+     console.log("hello")
+    //  div.classList.add("update")
+    div.style.transform += "rotate(45deg)"
+
+ })
